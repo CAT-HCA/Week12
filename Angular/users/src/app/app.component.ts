d1857dad7f317be41187d8184551a49800cdb360
Cori <corinne.a.ader@gmail.com>
2019-09-23 11:01:19 -0400

    Angular demos

---
+++ Angular/users/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Users';
+
+  // propName: type = defaultValue
+  firstName: string = 'Foo';
+  lastName: string = 'Bar';
+  email: string = 'foobar@test.com';
+  currentYear: number = 2019;
+
+  addNewUser: boolean = true;
+
+  // declare a method
+  getCurrentYear(): number {
+    return this.currentYear;
+  }
+}
